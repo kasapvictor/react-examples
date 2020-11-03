4c383f3150331f2cfebedbf3be1d5200434bfe0f
kasapvictor <kasap.victor@gmail.com>
2020-11-03 08:58:26 +0300

    hello

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
 import './styles/root.scss';
 
-export default () => (
-  <>
-    <h1>Welcome to React Parcel Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-  </>
-);
+export default class App extends React.Component {
+	constructor (props) {
+		super(props);
+		this.info = this.props.info;
+	}
+
+	render () {
+		return (
+			<>
+				<h1>{this.info}</h1>
+				<p>Hard to get more minimal than this React app.</p>
+			</>
+		);
+	}
+}
+
