626974b4cddc66ca1f2010f6dff0c2d9748f0d12
kasapvictor <kasap.victor@gmail.com>
2020-11-05 08:18:37 +0300

    clear

--- src/App.js
+++ src/App.js
@@ -1,43 +1,18 @@
 import React from "react";
 import './styles/root.scss';
-import {Country} from './components/Country';
-import {Town} from './components/Town';
 import { format } from "prettier";
 
 export default class App extends React.Component {
 	constructor (props) {
 		super(props);
-		this.info = this.props.info;
+		this.state = { }
 	}
 
 	render () {
 
-		const style = {
-			display: 'inline-block',
-			border: '1px solid tomato',
-			marginBottom: '10px'
-		}
-
 		return (
 			<>
-				<h1 style={style}> {this.info} </h1>
-				<h2>{this.props.info}</h2>
-				<p>Hard to get more minimal than this React app.</p>
-				
-				<div style={ {
-					color: 'tomato'
-				} }>
-					<Country 
-						info = {
-							{
-								countryName:'Canada', 
-								population:'100500'
-							}
-							}/>
-					<Town>
-						Monreal	
-					</Town>
-				</div>
+
 			</>
 		);
 	}
--- src/components/Town.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Town = ({children}) => {
-    return (
-        <div>
-            <p>{children}</p>
-        </div>
-    );
-}
