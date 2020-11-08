2afb0149488726be326059354019d714cc1bafad
kasapvictor <kasap.victor@gmail.com>
2020-11-08 07:26:25 +0300

    added states

--- src/App.js
+++ src/App.js
@@ -5,16 +5,24 @@ import './styles/root.scss';
 export default class App extends React.Component {
 	constructor (props) {
 		super(props);
-		this.state = { }
+		this.state = {
+			cars: [
+				{ name: 'Ford', 	year: 1984 },
+				{ name: 'Audi', 	year: 2016 },
+				{ name: 'Mazda', 	year: 2010 }
+			],
+			pageTitle: 'My cars'
+		}
 	}
 
 	render () {
+		const cars = this.state.cars;
 		return (
 			<div className="App">
-				<h1>Hello!</h1>
-				<Car name='Ford Mustang' year={1984} color='red'>
-					<p>Description: Lorem ipsum dolor sit amet.</p>
-				</Car>
+				<h1>{this.state.pageTitle}</h1>
+				<Car name={cars[0].name} year={cars[0].year}/>
+				<Car name={cars[1].name} year={cars[1].year}/>
+				<Car name={cars[2].name} year={cars[2].year}/>
 			</div>
 		);
 	}
