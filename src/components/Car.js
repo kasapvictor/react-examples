b003ab4e6c3ef699ab0c0009355495d4af69fad9
kasapvictor <kasap.victor@gmail.com>
2020-11-10 09:10:09 +0300

    Переключение стилей по условию

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,15 +1,33 @@
 import React from "react";
 import './Car.css';
 
-export default (props) => (
+export default (props) => {
+  const inputClasses = ['input'];
+
+  if (props.name !== '') {
+    inputClasses.push('green');
+  } else {
+    inputClasses.push('red');
+  }
+
+  if (props.name.length > 4) {
+    inputClasses.push('bold');
+  }
+
+  return (
     <div className="Car">
-      <h3>{props.name}</h3>
+      <h3>{ props.name }</h3>
       <ul>
-        <li>Year: {props.year}</li>
-        <li>Color: {props.color}</li>
+        <li>Year: { props.year }</li>
+        <li>Color: { props.color }</li>
       </ul>
-      <input type="text" onChange={props.onChangeName} value={props.name}/>
-      <button onClick={props.onDelete}>Delete</button>
+      <input
+        type="text"
+        onChange={ props.onChangeName }
+        value={ props.name }
+        className={inputClasses.join(' ')}
+      />
+      <button onClick={ props.onDelete }>Delete</button>
     </div>
   )
-
+}
