bc673aa262884e6beed64326ee832ab3a9097370
kasapvictor <kasap.victor@gmail.com>
2020-11-10 08:50:03 +0300

    добавление инлайн стилей

--- src/App.js
+++ src/App.js
@@ -22,8 +22,9 @@ export default class App extends React.Component {
 
 	onChangeName = (name, index) => {
 		const car = this.state.cars[index];
-		car.name = name;
 		const cars = [...this.state.cars];
+
+		car.name = name;
 		cars[index] = car;
 		this.setState({ cars });
 	}
@@ -37,27 +38,34 @@ export default class App extends React.Component {
 	}
 
 	render () {
+
+		let cars = null;
+
+		if (this.state.showCars) {
+				cars = this.state.cars.map((car, index ) => {
+					return <Car
+						key={index}
+						name={car.name}
+						year={car.year}
+						onChangeName={ event => this.onChangeName(event.target.value, index)}
+						onDelete={this.deleteHandler.bind(this, index)}
+					/>
+				})
+		}
+
 		return (
 			<div className="App">
 				<div>
 					<h1>{this.state.pageTitle}</h1>
-
 					<button onClick={ this.toggleCars }>Show cars</button>
 				</div>
-
-				{
-					this.state.showCars
-					? this.state.cars.map((car, index ) => {
-						return <Car
-										key={index}
-										name={car.name}
-										year={car.year}
-										onChangeName={ event => this.onChangeName(event.target.value, index)}
-										onDelete={this.deleteHandler.bind(this, index)}
-									/>
-					})
-					: null
-				}
+				<div style={{
+					width: 400,
+					margin: 'auto',
+					paddingTop: '20px'
+				}}>
+					{ cars }
+				</div>
 			</div>
 		);
 	}
--- src/components/Car.js
+++ src/components/Car.js
@@ -3,7 +3,10 @@ import React from "react";
 export default (props) => (
     <div style={{
       border: '1px solid #333',
-      marginBottom: '10px'
+      marginBottom: '10px',
+      padding: '20px',
+      boxShadow: '0 4px 5px 0 rgba(0,0,0,0.14)',
+      borderRadius: '5px'
     }}>
       <h3>{props.name}</h3>
       <ul>
